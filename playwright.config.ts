f00575f7512831df3fa6ebe43ada011e4a2c246b
Anoop Simon <Anoop.Simon@health.vic.gov.au>
2024-05-16 22:07:50 +1000

    added allure report integration

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
+import { testPlanFilter } from "allure-playwright/dist/testplan";
 
 /**
  * Read environment variables from file.
@@ -20,7 +21,25 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { outputFolder: 'playwright-report' }]],
+  reporter: [['html', { outputFolder: 'playwright-report' }],[
+    "allure-playwright",
+    {
+      detail: true,
+      outputFolder: "allure-results",
+      suiteTitle: true,
+      categories: [
+        {
+          name: "Outdated tests",
+          messageRegex: ".*FileNotFound.*",
+        },
+      ],
+      environmentInfo: {
+        framework: "playwright",
+      },
+    },
+  ],],
+  grep: testPlanFilter(),
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,21 @@
 import { test, expect } from '@playwright/test';
 import Commands from '../src/commands';
+import { Severity } from "allure-js-commons";
+import { allure } from "allure-playwright";
+
 const path = require('path');
 
 test.beforeEach(async ({ page }) => {
+  await allure.description(
+    "This test attempts to log into the website using a login and a password. Fails if any error happens.\n\nNote that this test does not test 2-Factor Authentication.",
+  );
+  await allure.owner("John Doe");
+  await allure.tags("NewUI", "Essentials", "Authentication");
+  await allure.severity(Severity.CRITICAL);
+  await allure.link("https://example.com/docs", "Related Documentation");
+  await allure.issue("AUTH-123", "https://example.com/issues/AUTH-123");
+  await allure.tms("TMS-456", "https://example.com/tms/TMS-456");
+
   global.app = new Commands(page);
   const filePath = path.resolve(__dirname, 'index.html');
 
