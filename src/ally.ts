98cc1a906c12eb26e4d3dafecc67ad580aa95c8f
Anoop Simon <Anoop.Simon@health.vic.gov.au>
2024-05-18 21:27:33 +1000

    updated tsconfig

--- src/ally.ts
+++ src/ally.ts
@@ -1,5 +1,6 @@
 import { Page } from 'playwright';
 import { injectAxe, checkA11y, configureAxe } from 'axe-playwright';
+import logger from './logger';
 
 interface AllyConfig {
   runOnly?: {
@@ -33,39 +34,49 @@ class Ally {
       reportFileName: 'accessibility-audit.html',
       ...config, // Merge default config with custom config
     };
+    logger.info('Ally instance created with configuration:', this.config);
   }
 
   async verify(page: Page, rules: any = null): Promise<void> {
-    // Inject axe-core library into the page
-    await injectAxe(page);
+    logger.info('Starting accessibility verification...');
+    try {
+      // Inject axe-core library into the page
+      await injectAxe(page);
+      logger.info('Injected axe-core library into the page');
 
-    // Configure axe if needed, e.g., setting custom rules or options
-    if (rules) {
-      await configureAxe(page, { rules });
-    }
-
-    // Run accessibility checks
-    await checkA11y(
-      page,
-      null,
-      {
-        axeOptions: {
-            runOnly: {
-                type: 'tag',
-                values: ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'],
-              }
-        },
-        detailedReport: this.config.detailedReport,
-        detailedReportOptions: this.config.detailedReportOptions,
-      },
-      true,
-      'html',
-      {
-        outputDirPath: this.config.outputDirPath,
-        outputDir: this.config.outputDir,
-        reportFileName: this.config.reportFileName,
+      // Configure axe if needed, e.g., setting custom rules or options
+      if (rules) {
+        await configureAxe(page, { rules });
+        logger.info('Configured axe with custom rules:', rules);
       }
-    );
+
+      // Run accessibility checks
+      await checkA11y(
+        page,
+        null,
+        {
+          axeOptions: {
+            runOnly: {
+              type: 'tag',
+              values: ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'],
+            },
+          },
+          detailedReport: this.config.detailedReport,
+          detailedReportOptions: this.config.detailedReportOptions,
+        },
+        true,
+        'html',
+        {
+          outputDirPath: this.config.outputDirPath,
+          outputDir: this.config.outputDir,
+          reportFileName: this.config.reportFileName,
+        }
+      );
+      logger.info('Accessibility check completed successfully');
+    } catch (error) {
+      logger.error('Error during accessibility verification:', error);
+      throw error;
+    }
   }
 }
 
--- tests/accessibilty.spec.ts
+++ tests/accessibilty.spec.ts
@@ -1,45 +1,12 @@
 import { test, expect } from '@playwright/test';
 import Ally from '../src/ally';
-const path = require('path');
+import path from 'path';
+const url = `file://${path.resolve(__dirname, 'apps/newspaper.html')}`;
 
 test('should have no accessibility violations', async ({ page }) => {
-  const filePath = path.resolve(__dirname, 'apps/newspaper.html');
-
-  // Open the local HTML file
-  await page.goto(`file://${filePath}`);
-
-  // Create an instance of Ally using the default configuration
   const ally = new Ally();
 
-  // Run accessibility verification
+  await page.goto(url);
   await ally.verify(page);
 
-  // Other test assertions...
-});
-
-test('should have no accessibility violations with custom configuration', async ({ page }) => {
-  const filePath = path.resolve(__dirname, 'apps/newspaper.html');
-
-  // Open the local HTML file
-  await page.goto(`file://${filePath}`);
-
-  // Create an instance of Ally with custom configuration
-  const ally = new Ally({
-    runOnly: {
-      type: 'tag',
-      values: ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'],
-    },
-    detailedReport: true,
-    detailedReportOptions: {
-      html: true,
-    },
-    outputDirPath: 'results',
-    outputDir: 'accessibility',
-    reportFileName: 'custom-accessibility-audit.html',
-  });
-
-  // Run accessibility verification
-  await ally.verify(page);
-
-  // Other test assertions...
 });
